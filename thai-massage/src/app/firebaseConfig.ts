935e6653e21e5448f2dbbfc5a4115919e9d81609
phhorsap <43028614+phhorsap@users.noreply.github.com>
2025-06-08 12:00:28 +0000

    config

--- thai-massage/src/app/firebaseConfig.ts
+++ thai-massage/src/app/firebaseConfig.ts
@@ -1,10 +1,4 @@
 export const firebaseConfig = {
-apiKey: "AIzaSyB83BFSnKS19M6zTQaLlFmodnF4hARr8k0",
-  authDomain: "thai-shop-ph.firebaseapp.com",
-  projectId: "thai-shop-ph",
-  storageBucket: "thai-shop-ph.firebasestorage.app",
-  messagingSenderId: "1006542050601",
-  appId: "1:1006542050601:web:e929b31d7be180189f9338",
-  measurementId: "G-HGZGF07H9C"
+
 
 };
